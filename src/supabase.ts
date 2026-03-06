e77419267ac37fb9564e3470f54fb76a42455ae0
martinez <mertinelli>
2026-03-06 21:32:47 +0100

    zmiana

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = import.meta.env.VITE_SUPABASE_URL;
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
-if (!url) throw new Error("Missing VITE_SUPABASE_URL");
-if (!key) throw new Error("Missing VITE_SUPABASE_ANON_KEY");
-
-export const supabase = createClient(url, key);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
